6b4f4b3d52eb7d3a07fe86b50deecdb153353eef
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-01-28 14:03:26 +0100

    make die time more recent

--- philo/death.c
+++ philo/death.c
@@ -6,14 +6,14 @@
 /*   By: dnoom <marvin@codam.nl>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/26 16:44:06 by dnoom         #+#    #+#                 */
-/*   Updated: 2022/01/28 12:00:07 by dnoom         ########   odam.nl         */
+/*   Updated: 2022/01/28 14:02:09 by dnoom         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <stdio.h>
 
-static int	die(t_shared *shared, t_philo *philo, long now)
+static int	die(t_shared *shared, t_philo *philo)
 {
 	shared->one_dead = 1;
 	if (ft_mutex_lock(&shared->print_butler))
@@ -21,7 +21,7 @@ static int	die(t_shared *shared, t_philo *philo, long now)
 	if (shared->allowed_to_print)
 	{
 		shared->allowed_to_print = 0;
-		printf("%ld %d died\n", now, philo->philo_i + 1);
+		printf("%ld %d died\n", get_time(), philo->philo_i + 1);
 	}
 	if (ft_mutex_unlock(&shared->print_butler))
 		return (ERROR);
@@ -35,7 +35,7 @@ int	check_death(t_shared *shared, t_philo *philo)
 	now = get_time();
 	if (now > philo->last_ate + shared->time_to_die)
 	{
-		die(shared, philo, now);
+		die(shared, philo);
 		return (ERROR);
 	}
 	return (SUCCESS);
